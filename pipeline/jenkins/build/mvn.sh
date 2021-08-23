21e4cef362e7863afa8f685fc59a455c202ceb41
ninad <ninad@ninad>
2021-08-23 11:48:56 +0000

    changed the .gitignore as one of the folders was called build and was getting ignored by git. changed .gitignore

---
+++ pipeline/jenkins/build/mvn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "********************************"
+echo "*** building jar ***************"
+echo "********************************"
+
+
+WORKSPACE=/home/azureuser/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/pipeline
+
+docker run -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn "$@"
